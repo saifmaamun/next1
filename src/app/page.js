d96f0576ccf1fae0ef0a24a30d148fa896a41c17
Saif Maamun <sadifmamun@gmail.com>
2024-12-30 22:54:52 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div >
+    <h1 className='text-9xl text-amber-600 text-center items-center'> welcome </h1>
+    </div>
+  );
+};
+
+export default HomePage;
