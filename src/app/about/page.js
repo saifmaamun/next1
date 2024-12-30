61fcc0fd5783d0c2394d018068e42b3f953f8de2
Saif Maamun <sadifmamun@gmail.com>
2024-12-30 23:38:37 +0600

    routing

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+       <h1 className='text-9xl text-amber-600 text-center items-center'> About </h1>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.js
@@ -0,0 +1,12 @@
+
+
+const page = () => {
+    return (
+        <div>
+            <h1 className='text-9xl text-amber-600 text-center items-center'> Contact </h1>
+            
+        </div>
+    );
+};
+
+export default page;
